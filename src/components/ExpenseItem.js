e077471cc3b7590156662552196c7895dca03bee
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-03-19 10:24:36 +0530

    writingcomplexjsx

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+      <div>
+        <div>Book</div>
+        <div>$10</div>
+        <div>15 August 2023</div>
+        <div>Delhi</div>
+      </div>
+    );
+  }
+  export default ExpenseItem;
