d381b31fbcd1cafe6a2cc5a32d64243342ad5fd1
varikuppalavinod <varikuppalavinod258@gmail.com>
2024-05-24 20:36:04 +0530

    reduxtoolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,11 @@
 import classes from './Counter.module.css';
 import {useSelector,useDispatch} from "react-redux";
-import {counterActions} from "../store/index"
+import {counterActions} from "../store/counter"
 
 
 const Counter = () => {
- const counter=useSelector((state)=>state.counter)
- const show=useSelector((state)=>state.showCounter)
+ const counter=useSelector((state)=>state.counter.counter)
+ const show=useSelector((state)=>state.counter.showCounter)
  const dispatch=useDispatch()
 
   const incrementhandler=()=>{
